cb6a2ea3c64583bcc94c023592e79b7bfc9d34ad
R.I.Pienaar <rip@devco.net>
2010-01-07 22:03:33 +0000

    make it into a SimpleRPC agent

--- agent/puppetd/puppetd.rb
+++ agent/puppetd/puppetd.rb
@@ -1,69 +1,53 @@
 module MCollective
     module Agent
-        class Puppetd
-            attr_reader :timeout, :meta
-
-            def initialize
-                @log = Log.instance
-                @config = Config.instance
+        class Puppetd<RPC::Agent
+            def startup_hook
+                meta[:license] = "Apache License 2.0"
+                meta[:author] = "R.I.Pienaar"
+                meta[:version] = "1.1"
+                meta[:url] = "http://mcollective-plugins.googlecode.com/"
 
                 @timeout = 10
-                @meta = {:license => "GPLv2",
-                         :author => "R.I.Pienaar <rip@devco.net>",
-                         :url => "http://code.google.com/p/mcollective-plugins/"}
 
-                @splaytime = 0
-                @lockfile = "/var/lib/puppet/state/puppetdlock"
-                @puppetd = "/usr/sbin/puppetd"
-
-                @splaytime = @config.pluginconf["puppetd.splaytime"].to_i if @config.pluginconf.include?("puppetd.splaytime")
-                @lockfile = @config.pluginconf["puppetd.lockfile"] if @config.pluginconf.include?("puppetd.lockfile")
-                @puppetd = @config.pluginconf["puppetd.puppetd"] if @config.pluginconf.include?("puppetd.puppetd")
+                @splaytime = @config.pluginconf["puppetd.splaytime"].to_i || 0
+                @lockfile = @config.pluginconf["puppetd.lockfile"] || "/var/lib/puppet/state/puppetdlock"
+                @puppetd = @config.pluginconf["puppetd.puppetd"] || "/usr/sbin/puppetd"
             end
 
-            def handlemsg(msg, connection)
-                command = msg[:body]
+            def enable_action
+                enable
+            end
 
-                ret = {"status" => false,
-                       "output" => "unknown command"}
-                
-                case command
-                    when "enable"
-                        ret = enable
+            def disable_action
+                disable
+            end
 
-                    when "disable"
-                        ret = disable
+            def runonce_action
+                runonce
+            end
 
-                    when "runonce"
-                        ret = runonce
-
-                    when "status"
-                        ret = status
-                end
-
-                ret
+            def status_action
+                status
             end
 
             def help
                 <<-EOH
-                Puppetd Agent
-                ==============
+                Simple RPC Puppetd Agent
+                ========================
     
                 Agent to enable, disable and run the puppet agent
     
-                Accepted Messages
-                -----------------
-                Input is a simple string, can be:
-    
-                enable     - Deletes the lock file if present
-                disable    - Disable the puppetd using --disable if possible
-                runonce    - Run the puppetd with --runonce, if configured with 
-                             it will splay it with that amount.
-                status     - Always returns true, but with some text about the
-                             current status of your daemon.
-    
+                ACTIONS:
+                    enable, disable, status, runonce
 
-                Configuration 
+                INPUT:
+                    none
+
+                OUTPUT:
+                    :output     A string showing some human parsable status
+                    :enabled    for the status action, 1 if the daemon is enabled, 0 otherwise
+
+                CONFIGURATION 
                 -------------
 
                 puppetd.splaytime - How long to splay for, no splay by default
@@ -71,87 +55,59 @@ module MCollective
                                     /var/lib/puppet/state/puppetdlock
                 puppetd.puppetd   - Where to find the puppetd, defaults to 
                                     /usr/sbin/puppetd
-
-                Returned Data
-                -------------
-    
-                Returned data is a hash, status is boolean indicating success of the request 
-                while the output is just some pretty text.
-    
-                {"status" => true,
-                 "output" => "Already running"}
                 EOH
             end
 
             private
             def status
-                ret = {"status" => true,
-                       "output" => "Enabled, not running"}
-
                 if File.exists?(@lockfile)
                     if File::Stat.new(@lockfile).zero?
-                        ret["output"] = "Disabled, not running"
+                        reply[:output] = "Disabled, not running"
+                        reply[:enabled] = 0
                     else
-                        ret["output"] = "Enabled, running"
+                        reply[:output] = "Enabled, running"
+                        reply[:enabled] = 1
                     end
                 end
-
-                ret
             end
 
             def runonce
-                ret = {}
-
                 if File.exists?(@lockfile)
-                    ret = {"status" => false,
-                           "output" => "Lock file exists"}
+                    reply.fail "Lock file exists"
                 else
                     if @splaytime > 0
-                        ret = {"status" => true,
-                               "output" => %x[#{@puppetd} --onetime --splaylimit #{@splaytime} --splay]}
+                        reply[:output] = %x[#{@puppetd} --onetime --splaylimit #{@splaytime} --splay]
                     else
-                        ret = {"status" => true,
-                               "output" => %x[#{@puppetd} --onetime]}
+                        reply[:output] = %x[#{@puppetd} --onetime]
                     end
                 end
-
-                ret
             end
 
             def enable
-                ret = {"status" => true,
-                       "output" => "Already enabled"}
-
                 if File.exists?(@lockfile)
                     stat = File::Stat.new(@lockfile)
 
                     if stat.zero?
                         File.unlink(@lockfile)
-                        ret = {"status" => true,
-                               "output" => "Lock removed"}
+                        reply[:output] = "Lock removed"
                     else
-                        ret = {"status" => true,
-                               "output" => "Currently runing"}
+                        reply[:output] = "Currently runing"
                     end
+                else
+                    reply.fail "Already unlocked"
                 end
-
-                ret
             end
 
             def disable
-                ret = {"status" => false,
-                       "output" => "Disabled or already running"}
-                
                 if File.exists?(@lockfile)
                     stat = File::Stat.new(@lockfile)
 
-                    stat.zero? ? ret["output"] = "Already disabled" : ret["output"] = "Currently running"
+                    stat.zero? ? reply.fail("Already disabled") : reply.fail("Currently running")
                 else
-                    ret = {"status" => true,
-                           "output" => %x[#{@puppetd} --disable]}
-                end
+                    reply[:output] = %x[#{@puppetd} --disable]
 
-                ret
+                    reply[:output] = "Lock created" if reply[:output] == ""
+                end
             end
         end
     end
