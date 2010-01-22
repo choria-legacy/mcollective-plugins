5574ef5e4aef45cac392c8b78c9c0358b28249bf
R.I.Pienaar <rip@devco.net>
2010-01-22 00:12:08 +0000

    rename

--- audit/centralrpclog/audit/centrallog.rb
+++ audit/centralrpclog/audit/centrallog.rb
@@ -1,13 +1,13 @@
 module MCollective
     module RPC
         # A RPC::Audit plugin that sends all audit messages to a non SimpleRPC agent called
-        # centrallog where it can then process them however it feels like
-        class Centrallog<Audit
+        # centralrpclog where it can then process them however it feels like
+        class Centralrpclog<Audit
             def audit_request(request, connection)
                 config = Config.instance
-                target = Util.make_target("centrallog", :command)
+                target = Util.make_target("centralrpclog", :command)
                 reqid = Digest::MD5.hexdigest("#{config.identity}-#{Time.now.to_f.to_s}-#{target}")
-                filter = {"agent" => "centrallog"}
+                filter = {"agent" => "centralrpclog"}
 
                 req = PluginManager["security_plugin"].encoderequest(config.identity, target, request, reqid, filter)
 
