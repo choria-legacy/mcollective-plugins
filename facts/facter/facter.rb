8de3500f471acc059fb634f274166c476c076e3e
Mike Pountney <Mike.Pountney@gmail.com>
2010-04-30 11:39:29 +0000

    Update issue 3: add config parms for cache_time and facterlib, convert to use FACTERLIB over LOAD_PATH

--- facts/facter/facter.rb
+++ facts/facter/facter.rb
@@ -4,13 +4,15 @@ module MCollective
 
         # A factsource for Reductive Labs Facter
         #
-        # This plugin currently assumes you put custom facts via
-        # Puppets pluginsync in /var/lib/puppet/lib you can just edit
-        # the path for now if your custom facts are elsewhere.
+        # This plugin by default works with puppet facts loaded via pluginsync
+        # and the deprecated factsync. If your facts are in a custom location, or
+        # you use non-standard puppet dirs, then set plugin.facter.facterlib 
+        # in the server.cfg
         #
         # It caches facts for 300 seconds to speed things up a bit,
         # generally though using this plugin will slow down discovery by
         # a second or so.
+        #   - the cache time can be altered by setting plugin.facter.cache_time in the server.cfg
         #
         # See: http://code.google.com/p/mcollective-plugins/wiki/FactsRLFacter
         #
@@ -19,13 +21,19 @@ module MCollective
             @@last_facts_load = 0
 
             def get_facts
-                unless $LOAD_PATH.include?("/var/lib/puppet/lib")
-                    $LOAD_PATH << "/var/lib/puppet/lib"
-                end
-    
+
+                config = Config.instance
+                logger = Log.instance
+
+                ENV['FACTERLIB'] = config.pluginconf["facter.facterlib"] || "/var/lib/puppet/lib/facter:/var/lib/puppet/facts"
+                cache_time = config.pluginconf["facter.cache_time"] || 300
+
+                logger.debug("Have FACTERLIB: #{ENV['FACTERLIB']}")
+
                 begin 
-                    if (Time.now.to_i - @@last_facts_load > 300)
+                    if (Time.now.to_i - @@last_facts_load > cache_time.to_i )
                         @@last_facts_load = Time.now.to_i
+                        logger.debug("Resetting facter cache after #{cache_time} seconds")
                         ::Facter.reset
                     end
                 rescue
