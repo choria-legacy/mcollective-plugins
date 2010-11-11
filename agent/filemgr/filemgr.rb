29d5870877ba3f40a052c3ce975f34827c37c4a8
R.I.Pienaar <rip@devco.net>
2010-11-11 19:17:20 +0000

    Refactor the filemgr plugin to be more like newer mcollective plugins
    Extend the status action significantly with new status information like
    everything from File#stat and also md5 sums etc

--- agent/filemgr/filemgr.rb
+++ agent/filemgr/filemgr.rb
@@ -1,44 +1,38 @@
 require 'fileutils'
+require 'digest/md5'
 
 module MCollective
     module Agent
+        # A basic file management agent, you can touch, remove or inspec files.
+        #
+        # A common use case for this plugin is to test your mcollective setup
+        # as such if you just call the touch/info/remove actions with no arguments
+        # it will default to the file /var/run/mcollective.plugin.filemgr.touch
+        # or whatever is specified in the plugin.filemgr.touch_file setting
         class Filemgr<RPC::Agent
-
-            def startup_hook
-                meta[:license] = "Apache 2"
-
-                meta[:author] = "Mike Pountney"
-                meta[:version] = "0.2"
-                meta[:url] = "http://mcollective.googlecode.com/"
-
-                @timeout = 5
-            end
+            metadata    :name        => "filemgr",
+                        :description => "File Manager",
+                        :author      => "Mike Pountney <mike.pountney@gmail.com>",
+                        :license     => "Apache 2",
+                        :version     => "0.3",
+                        :url         => "http://www.puppetlabs.com/mcollective",
+                        :timeout     => 5
 
             # Basic file touch action - create (empty) file if it doesn't exist,
             # update last mod time otherwise.
             # useful for checking if mcollective is operational, via NRPE or similar.
-            def touch_action
+            action "touch" do
                 touch
             end
- 
+
             # Basic file removal action
-            def remove_action
+            action "remove" do
                 remove
             end
 
-            # Basic file removal action
-            def status_action
+            # Basic status of a file
+            action "status" do
                 status
-            end
-
-            def help
-                <<-EOH
-                Basic file handling agent:
-
-                Actions:
-                  touch - update last_modified (or create). Accepts :file (defaults to plugin.filemgr.touch_file or '/var/run/mcollective.plugin.filemgr.touch')
-                  remove - remove a single file, does not error if nonexistant. Accepts :file
-                EOH
             end
 
             private
@@ -48,15 +42,50 @@ module MCollective
 
             def status
                 file = get_filename
+                reply[:name] = file
                 reply[:output] = "not present"
+                reply[:type] = "unknown"
+                reply[:mode] = "0000"
                 reply[:present] = 0
+                reply[:size] = 0
+                reply[:mtime] = 0
+                reply[:ctime] = 0
+                reply[:atime] = 0
+                reply[:mtime_seconds] = 0
+                reply[:ctime_seconds] = 0
+                reply[:atime_seconds] = 0
+                reply[:md5] = 0
+                reply[:uid] = 0
+                reply[:gid] = 0
+
 
                 if File.exists?(file)
                     logger.debug("Asked for status of '#{file}' - it is present")
                     reply[:output] = "present"
                     reply[:present] = 1
+
+                    stat = File.stat(file)
+
+                    [:size, :mtime, :ctime, :atime, :uid, :gid].each do |item|
+                        reply[item] = stat.send(item)
+                    end
+
+                    [:mtime, :ctime, :atime].each do |item|
+                        reply["#{item}_seconds".to_sym] = stat.send(item).to_i
+                    end
+
+                    reply[:mode] = "%o" % [stat.mode]
+                    reply[:md5] = Digest::MD5.hexdigest(File.read(file)) if stat.file?
+
+                    reply[:type] = "directory" if stat.directory?
+                    reply[:type] = "file" if stat.file?
+                    reply[:type] = "symlink" if stat.symlink?
+                    reply[:type] = "socket" if stat.socket?
+                    reply[:type] = "chardev" if stat.chardev?
+                    reply[:type] = "blockdev" if stat.blockdev?
                 else
                     logger.debug("Asked for status of '#{file}' - it is not present")
+                    reply.fail! "#{file} does not exist"
                 end
             end
 
@@ -65,7 +94,6 @@ module MCollective
                 if ! File.exists?(file)
                     logger.debug("Asked to remove file '#{file}', but it does not exist")
                     reply.statusmsg = "OK"
-                    return
                 end
 
                 begin
@@ -74,10 +102,8 @@ module MCollective
                     reply.statusmsg = "OK"
                 rescue
                     logger.warn("Could not remove file '#{file}'")
-                    reply.fail "Could not remove file '#{file}'", 1 
+                    reply.fail! "Could not remove file '#{file}'"
                 end
-
-                return
             end
 
             def touch
@@ -87,11 +113,9 @@ module MCollective
                     logger.debug("Touched file '#{file}'")
                 rescue
                     logger.warn("Could not touch file '#{file}'")
-                    reply.fail "Could not touch file '#{file}'", 1 
+                    reply.fail! "Could not touch file '#{file}'"
                 end
-                return
             end
-
         end
     end
 end
