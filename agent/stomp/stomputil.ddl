3c5670e28209f82e3e88b4e9ef6b52bdeac67d40
R.I.Pienaar <rip@devco.net>
2010-12-18 16:13:09 +0000

    Add the stomputil and mc-peermap client

---
+++ agent/stomp/stomputil.ddl
@@ -0,0 +1,27 @@
+metadata    :name        => "STOMP Connector Utility Agent",
+            :description => "Various helpers and useful actions for the STOMP connector",
+            :author      => "R.I.Pienaar <rip@devco.net>",
+            :license     => "Apache v 2.0",
+            :version     => "1.0",
+            :url         => "http://projects.puppetlabs.com/projects/mcollective-plugins/wiki",
+            :timeout     => 5
+
+action "peer_info", :description => "Get STOMP Connection Peer" do
+    display :always
+
+    output :protocol,
+           :description => "IP Protocol in use",
+           :display_as => "Protocol"
+
+    output :destport,
+           :description => "Destination Port",
+           :display_as => "Port"
+
+    output :desthost,
+           :description => "Destination Host",
+           :display_as => "Host"
+
+    output :destaddr,
+           :description => "Destination Address",
+           :display_as => "Address"
+end
---
+++ agent/stomp/stomputil.rb
@@ -0,0 +1,30 @@
+module MCollective
+    module Agent
+        class Stomputil<RPC::Agent
+            metadata    :name        => "STOMP Connector Utility Agent",
+                        :description => "Various helpers and useful actions for the STOMP connector",
+                        :author      => "R.I.Pienaar <rip@devco.net>",
+                        :license     => "Apache v 2.0",
+                        :version     => "1.0",
+                        :url         => "http://projects.puppetlabs.com/projects/mcollective-plugins/wiki",
+                        :timeout     => 5
+
+            # Get the Stomp connection peer information
+            action "peer_info" do
+                peer = PluginManager["connector_plugin"].connection.socket.peeraddr
+
+                reply[:protocol] = peer[0]
+                reply[:destport] = peer[1]
+                reply[:desthost] = peer[2]
+                reply[:destaddr] = peer[3]
+            end
+
+            private
+            def get_pid(process)
+                pid = `pidof #{process}`.chomp.grep(/\d+/)
+
+                pid.first
+            end
+        end
+    end
+end
