ba334dc698eb084c1d770075e26f594efc6cbf9e
Dean Wilson <dean.wilson@gmail.com>
2011-02-02 13:11:38 +0000

    Add an easy way to see which machines are in violation.

--- agent/registration-monitor/check_mcollective.rb
+++ agent/registration-monitor/check_mcollective.rb
@@ -9,13 +9,15 @@ require 'getoptlong'
 
 opts = GetoptLong.new(
     [ '--directory', '-d', GetoptLong::REQUIRED_ARGUMENT],
-    [ '--interval', '-i', GetoptLong::REQUIRED_ARGUMENT]
+    [ '--interval', '-i', GetoptLong::REQUIRED_ARGUMENT],
+    [ '--verbose', '-v',  GetoptLong::NO_ARGUMENT]
 )
 
 dir = "/var/tmp/mcollective"
 interval = 300
 total = 0
 old = 0
+verbose = false
 
 opts.each do |opt, arg|
     case opt
@@ -23,8 +25,12 @@ opts.each do |opt, arg|
             dir = arg
         when '--interval'
             interval = arg.to_i
+        when '--verbose'
+            verbose = true
     end
 end
+
+hosts = [ ]
 
 Dir.open(dir) do |files|
     files.each do |f|
@@ -35,13 +41,20 @@ Dir.open(dir) do |files|
         total += 1
 
         if (Time.now.to_i - fage) > interval + 30
+            hosts.push f if verbose
             old += 1
         end
     end
 end
 
 if old > 0
-    puts("CRITICAL: #{old} / #{total} hosts not checked in within #{interval} seconds| totalhosts=#{total} oldhosts=#{old} currenthosts=#{total - old}")
+    if verbose
+        failed = hosts.join(', ')
+        puts("CRITICAL: #{old} / #{total} hosts not checked in within #{interval} seconds - failed: #{failed}| totalhosts=#{total} oldhosts=#{old} currenthosts=#{total - old}")
+    else
+        puts("CRITICAL: #{old} / #{total} hosts not checked in within #{interval} seconds| totalhosts=#{total} oldhosts=#{old} currenthosts=#{total - old}")
+    end
+
     exit 2
 else
     puts("OK: #{total} / #{total} hosts checked in within #{interval} seconds| totalhosts=#{total} oldhosts=#{old} currenthosts=#{total - old}")
