df353e875ef6d7eba9e8a248546e0d3dfe824e65
Pieter Loubser <ploubser@gmail.com>
2011-07-22 16:54:34 +0100

    7817 - mc-nrpe should work with monolithic config files
    The nrpe agent will now firstly look for a nrpe config directory
    in pluginconf before assuming /etc/nagios/nrpe.d to be the config
    dir. A monolithic config file can now also be specified.

--- agent/nrpe/agent/nrpe.rb
+++ agent/nrpe/agent/nrpe.rb
@@ -5,7 +5,7 @@ module MCollective
                         :description => "Agent to query NRPE commands via MCollective",
                         :author      => "R.I.Pienaar",
                         :license     => "Apache 2",
-                        :version     => "1.3",
+                        :version     => "2.0",
                         :url         => "http://projects.puppetlabs.com/projects/mcollective-plugins/wiki",
                         :timeout     => 5
 
@@ -56,15 +56,24 @@ module MCollective
             private
             def plugin_for_command(req)
                 ret = nil
+                fname = nil
 
                 fdir  = config.pluginconf["nrpe.conf_dir"] || "/etc/nagios/nrpe.d"
-                fname = "#{fdir}/#{req}.cfg"
+
+                if config.pluginconf["nrpe.conf_file"]
+                    fname = "#{fdir}/#{config.pluginconf['nrpe.conf_file']}"
+                else
+                    fname = "#{fdir}/#{req}.cfg"
+                end
 
                 if File.exist?(fname)
-                    t = File.readlines(fname).first.chomp
+                    t = File.readlines(fname)
+                    t.each do |check|
+                        check.chomp!
 
-                    if t =~ /command\[.+\]=(.+)$/
-                        ret = {:cmd => $1}
+                        if check =~ /command\[#{request[:command]}\]=(.+)$/
+                            ret = {:cmd => $1}
+                        end
                     end
                 end
 
---
+++ agent/nrpe/spec/nrpe_agent_spec.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+
+require File.join([File.dirname(__FILE__), '/../../../spec/spec_helper'])
+
+describe "nrpe agent" do
+    before do
+        agent_file = File.join([File.dirname(__FILE__), "../agent/nrpe.rb"])
+        @agent = MCollective::Test::LocalAgentTest.new("nrpe", :agent_file => agent_file).plugin
+    end
+
+    describe "#metda" do
+        it "should have valid metadata" do
+            @agent.should have_valid_metadata
+        end
+    end
+
+    describe "#runcommand" do
+        it "should reply with statusmessage 'OK' of exitcode is 0" do
+            @agent.expects(:plugin_for_command).with("foo").returns:cmd => ("foo")
+            @agent.expects(:run).with("foo", :stdout => :output, :chomp => true).returns(0)
+            result = @agent.call(:runcommand, :command => "foo")
+            result.should be_successful
+            result.should have_data_items(:exitcode=>0, :perfdata=>"")
+            result[:statusmsg].should == "OK"
+        end
+
+        it "should reply with statusmessage 'WARNING' of exitcode is 1" do
+            @agent.expects(:plugin_for_command).with("foo").returns:cmd => ("foo")
+            @agent.expects(:run).with("foo", :stdout => :output, :chomp => true).returns(1)
+            result = @agent.call(:runcommand, :command => "foo")
+            result.should be_aborted_error
+            result.should have_data_items(:exitcode=>1, :perfdata=>"")
+            result[:statusmsg].should == "WARNING"
+        end
+
+        it "should reply with statusmessage 'CRITICAL' of exitcode is 2" do
+            @agent.expects(:plugin_for_command).with("foo").returns:cmd => ("foo")
+            @agent.expects(:run).with("foo", :stdout => :output, :chomp => true).returns(2)
+            result = @agent.call(:runcommand, :command => "foo")
+            result.should be_aborted_error
+            result.should have_data_items(:exitcode=>2, :perfdata=>"")
+            result[:statusmsg].should == "CRITICAL"
+        end
+
+        it "should reply with statusmessage UKNOWN if exitcode is something else" do
+            @agent.expects(:plugin_for_command).with("foo").returns:cmd => ("foo")
+            @agent.expects(:run).with("foo", :stdout => :output, :chomp => true)
+            result = @agent.call(:runcommand, :command => "foo")
+            result.should be_aborted_error
+            result.should have_data_items(:exitcode=>nil, :perfdata=>"")
+            result[:statusmsg].should == "UNKNOWN"
+        end
+
+        it "should fail on an unknown command" do
+            @agent.expects(:plugin_for_command).with("foo").returns(nil)
+            result = @agent.call(:runcommand, :command => "foo")
+            result.should be_aborted_error
+            result.should have_data_items(:output => "No such command: foo", :exitcode => 3)
+        end
+    end
+
+    describe "#plugin_for_command" do
+        it "should return the command from nrpe.conf_dir if it is set" do
+            @agent.config.stubs(:pluginconf).returns("nrpe.conf_dir" => "/foo", "nrpe.conf_file" => "bar.cfg")
+            @agent.stubs(:request).returns(:command => "command")
+            File.expects(:exist?).with("/foo/bar.cfg").returns(true)
+            File.expects(:readlines).with("/foo/bar.cfg").returns(["command[command]=run"])
+            @agent.expects(:run).with("run", :stdout => :output, :chomp => true).returns(0)
+            result = @agent.call(:runcommand, :command => "run")
+            result.should be_successful
+            result.should have_data_items(:exitcode=>0, :perfdata=>"")
+            result[:statusmsg].should == "OK"
+        end
+
+        it "should return the command from /etc/nagios/nrpe.d if nrpe.conf_dir is unset" do
+            @agent.config.stubs(:pluginconf).returns("")
+            @agent.stubs(:request).returns(:command => "command")
+            File.expects(:exist?).with("/etc/nagios/nrpe.d/command.cfg").returns(true)
+            File.expects(:readlines).with("/etc/nagios/nrpe.d/command.cfg").returns(["command[command]=run"])
+            @agent.expects(:run).with("run", :stdout => :output, :chomp => true).returns(0)
+            result = @agent.call(:runcommand, :command => "run")
+            result.should be_successful
+            result.should have_data_items(:exitcode=>0, :perfdata=>"")
+            result[:statusmsg].should == "OK"
+        end
+
+    end
+
+end
